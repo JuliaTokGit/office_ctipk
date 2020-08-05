a26c03ce7b34da7eee23f6f7947691a281a86c21
Alex <shuricken@gmail.com>
2020-08-05 20:17:36 +0500

    fix

--- app/models/Order.php
+++ app/models/Order.php
@@ -1,6 +1,6 @@
 <?php
 use Illuminate\Database\Eloquent\Model;
-use CastDate;
+use Carbon\Carbon;
 
 class Order extends Model
 {
@@ -13,14 +13,70 @@ class Order extends Model
     public $timestamps = false;
     protected $dates = [
         'Дата_Заявки',
+        'Дата_Рождения',
+        'Дата_Оплата',
+        'Дата_Доплата',
+        'Дата_Техник',
+        'Дата_Исполнения',
+        'Дата_Задания',
+        'Дата_Архив',
+        'Дата_Оплаты_Дог',
+        'Дата_Выдачи',
+        'Дата_Выдачи_Заказчику',
+        'Дата_Договора',
+        'Дата_Переноса',
     ];
-    // protected $dateFormat = 'Y-m-d H:i:s.v';
-    protected $casts = [
-        'Дата_Заявки' => 'CastDate::class',
-    ];
-    // public function getDateFormat()
-    // {
-    //     return 'Y-d-m H:i:s.v';
-    // }
+    protected $dateFormat = 'Y-m-d H:i:s.v';
 
+    public function getДатаЗаявкиAttribute($value){
+        return Carbon::createFromFormat('Y-m-d H:i:s.v', $value)->format('Y-m-d');
+    }
+
+    public function getДатаРожденияAttribute($value){
+        return Carbon::createFromFormat('Y-m-d H:i:s.v', $value)->format('Y-m-d');
+    }
+
+    public function getДатаОплатаAttribute($value){
+        return Carbon::createFromFormat('Y-m-d H:i:s.v', $value)->format('Y-m-d');
+    }
+
+    public function getДатаДоплатаAttribute($value){
+        return Carbon::createFromFormat('Y-m-d H:i:s.v', $value)->format('Y-m-d');
+    }
+
+    public function getДатаТехникAttribute($value){
+        return Carbon::createFromFormat('Y-m-d H:i:s.v', $value)->format('Y-m-d');
+    }
+
+    public function getДатаИсполненияAttribute($value){
+        return Carbon::createFromFormat('Y-m-d H:i:s.v', $value)->format('Y-m-d');
+    }
+
+    public function getДатаЗаданияAttribute($value){
+        return Carbon::createFromFormat('Y-m-d H:i:s.v', $value)->format('Y-m-d');
+    }
+
+    public function getДатаАрхивAttribute($value){
+        return Carbon::createFromFormat('Y-m-d H:i:s.v', $value)->format('Y-m-d');
+    }
+
+    public function getДатаОплатыДогAttribute($value){
+        return Carbon::createFromFormat('Y-m-d H:i:s.v', $value)->format('Y-m-d');
+    }
+    
+    public function getДатаВыдачиAttribute($value){
+        return Carbon::createFromFormat('Y-m-d H:i:s.v', $value)->format('Y-m-d');
+    }
+
+    public function getДатаВыдачиЗаказчикуAttribute($value){
+        return Carbon::createFromFormat('Y-m-d H:i:s.v', $value)->format('Y-m-d');
+    }
+
+    public function getДатаДоговораAttribute($value){
+        return Carbon::createFromFormat('Y-m-d H:i:s.v', $value)->format('Y-m-d');
+    }
+
+    public function getДатаПереносаAttribute($value){
+        return Carbon::createFromFormat('Y-m-d H:i:s.v', $value)->format('Y-m-d');
+    }
 }
