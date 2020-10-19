5be44ba538db37b98bda1088e8cf1cf1618a2921
Кужельный Юрий <kuzhelnyi@ctipk.ru>
2020-10-19 14:07:08 +0500

    Sync

--- app/views/assets/scripts/order.js
+++ app/views/assets/scripts/order.js
@@ -2,4 +2,9 @@ $(function(){
   $( ".select_load" ).each(function() {    
     $( this ).val($(this).data("value")).trigger("change");
   });
-})
+});
+$('input[name="exampleRadios"]').on('click', function(e) {
+$(".switched_forms").hide();
+$($(this).val()).show();
+
+});
--- app/views/assets/scripts/performers-orders.js
+++ app/views/assets/scripts/performers-orders.js
@@ -1,15 +1,15 @@
 columns=[
-    { data: "Код_Исполнителя", "name":"Код_исполнителя",  visible:false,  },
-    { data: "Код_Заявки", "name":"Код_Заявки", visible: false },
-
-
+    { data: "Код_Исполнителя", "name":"Код_исполнителя", title:"Код исполнителя", visible:true,  },
+    { data: "Код_Заявки", "name":"Код_Заявки",title:"Код заявки", visible: true },
     { data: "Исполнитель", "name":"Исполнитель", title:"Исполнитель", defaultContent:"" },
-    { data: "Вид_Работы", "name":"Вид_Работы", title:"Вид_Работы", visible: false  },
-    { data: "Вид_Инвентаризации", "name":"Вид_Инвентаризации", visible: false  },
+    { data: "Вид_Работы", "name":"Вид_Работы", title:"Вид Работы", visible: true  },
+    { data: "Вид_Инвентаризации", "name":"Вид_Инвентаризации", title:"Вид инвентаризации",visible: true  },
     { data: "Наименование_Работы", "name":"Наименование_Работы", title:"Наименование работы", defaultContent:""  },
+    { data: "Примечание", "name":"Примечание", title:"Примечание", defaultContent:""},
     { data: "Процент_Выручка", "name":"Процент_Выручка", title:"Процент", defaultContent:""},
     { data: "Доля_Выручка", "name":"Доля_Выручка", title:"Доля", defaultContent:""},
     {data: "Адрес_Объекта", "name":"Адрес_Объекта",title: "Адрес объекта",defaultContent: "" },
-    { data: "Примечание", "name":"Примечание", title:"Примечание", defaultContent:""}
+    {data:"Отдел","name":"Отдел",title: "Отдел",defaultContent: ""}
+
 
 ];
