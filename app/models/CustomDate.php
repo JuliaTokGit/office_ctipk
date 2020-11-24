aa200581ffdb402eb7c5f0a49296b6169caa2a32
Alex <shuricken@gmail.com>
2020-11-24 10:26:46 +0500

    fix

--- app/models/CustomDate.php
+++ app/models/CustomDate.php
@@ -6,6 +6,7 @@ class CustomDate implements CastsAttributes
 {
     public function get($model, $key, $value, $attributes)
     {      
+        $value=str_replace('T',' ',$value);
         return empty($value)?'':Carbon::createFromFormat('Y-m-d H:i:s.v', $value)->format('Y-m-d');
     }
 
@@ -20,8 +21,8 @@ class CustomDate implements CastsAttributes
             // $date = new DateTime("now", $timezone);
             // $result=$date->format("Y-m-d\TH:i:s");
             // $result=;
-            // dd($result);
-            return [$key => Carbon::createFromFormat('Y-m-d', $value)->format('Y-m-d H:i:s.v')];
+            // dd(Carbon::createFromFormat('Y-m-d', $value)->format('Y-m-d\TH:i:s.v'));
+            return [$key => Carbon::createFromFormat('Y-m-d', $value)->format('Y-m-d\TH:i:s.v')];
         }
         
     }
